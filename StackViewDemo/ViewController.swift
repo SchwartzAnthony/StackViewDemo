6ce1b068e3879f1ce9d85d83980ad1a90690a49f
Anthony Schwartz <anthonyschwartz@anthonys-imac.local>
2018-06-11 12:17:35 -0500

    Initial Commit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Anthony Schwartz on 6/11/18.
+//  Copyright © 2018 Anthony Schwartz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
